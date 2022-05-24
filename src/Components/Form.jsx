7ba6eb2590842bdb1f90558c7d21146a989b1285
Abhishek Pandey <33335446+EnRyuuX7@users.noreply.github.com>
2022-05-24 11:17:22 +0545

    initial commit

---
+++ src/Components/Form.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+function Form() {
+    const { register, handleSubmit, watch } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(watch("example"));
+
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("firstName", { required: true, maxLength: 20 })} />
+            <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+            <input type="number" {...register("age", { min: 18, max: 99 })} />
+            <input type="submit" />
+        </form>
+    )
+}
+
+export default Form
